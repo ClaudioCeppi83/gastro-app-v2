d07b6707eef2e52e66be1da326eb11852068086a
erceppi <erceppi@gmail.com>
2025-04-19 16:18:20 +0000

    E.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,10 +1,11 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
-{ ... }: {
+{ pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+    pkgs.adoptopenjdk-openj9-bin-11
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
