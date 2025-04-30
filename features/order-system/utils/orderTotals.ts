9599fb25e313bdec8d77230dcce9cd630aa6ccd0
Claudio Ceppi <erceppi@gmail.com>
2025-04-30 21:24:17 +0200

    E.

--- features/order-system/utils/orderTotals.ts
+++
@@ -1,7 +0,0 @@
-export const calculateOrderTotal = (
-  subtotal: number,
-  iva: number,
-  serviceCharge: number,
-): number => {
-  return subtotal + iva + serviceCharge;
-};
--- features/order-system/utils/orderValidations.ts
+++
@@ -1,8 +0,0 @@
-import { OrderItem } from '../types';
-
-export const validateOrderItem = (item: OrderItem): string[] => {
-  const errors = [];
-  if (item.quantity <= 0) errors.push('Cantidad inválida');
-  if (!item.dishId) errors.push('Plato requerido');
-  return errors;
-};
